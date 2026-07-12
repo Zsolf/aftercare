06ed40eca4c666a485ff6466cd89ed2898df83cb
zsolf <zsolf57@gmail.com>
2026-07-12 13:53:08 +0200

    init

---
+++ src/main/java/com/zsolf/aftercare/AftercareApplication.java
@@ -0,0 +1,13 @@
+package com.zsolf.aftercare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftercareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftercareApplication.class, args);
+    }
+
+}
